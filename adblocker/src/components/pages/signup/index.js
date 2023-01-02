d828fa3e9a66e7427fddf4be9a3dbfd4bc7cdd18
Oleksii Honchar <alekseyhonchar@gmail.com>
2023-01-02 13:18:04 +0000

    feat: done signup page

---
+++ adblocker/src/components/pages/signup/index.js
@@ -0,0 +1,101 @@
+import { useState } from 'react';
+import { Container, Box,
+  Avatar, Typography,
+  Grid, TextField, Button, Link } from '@mui/material';
+import * as EmailValidator from 'email-validator';
+
+export default function SignUp() {
+  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+  const [email, setEmail] = useState({ value: '', isValid: true });
+  const [password, setPassword] = useState({ value: '', isValid: true });
+  const [confirmPassword, setConfirmPassword] = useState({ value: '', isValid: true });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    
+    if (email.isValid && password.isValid && confirmPassword.isValid) {
+      console.log({
+        email: data.get('email'),
+        password: data.get('password'),
+      });
+    }
+    
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }} />
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid item sx={{marginTop: 2}}>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                error={!email.isValid}
+                helperText={email.isValid ? "" : "Email is not valid"}
+                onChange={e => setEmail({ value: e.target.value, isValid: EmailValidator.validate(e.target.value) })}
+              />
+            </Grid>
+            <Grid item sx={{marginTop: 2}}>
+              <TextField
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                error={!password.isValid}
+                helperText={password.isValid ? "" : "Password should have minimum 8 characters, at least 1 letter and 1 number"}
+                onChange={e => {
+                  const newPassword = e.target.value;
+                  setPassword({ value: newPassword, isValid: newPassword.match(passwordRegex) })
+                  setConfirmPassword({ value: confirmPassword.value, isValid: confirmPassword.value === newPassword})
+                }}
+              />
+            </Grid>
+            <Grid item sx={{marginTop: 2}}>
+              <TextField
+                required
+                fullWidth
+                name="password"
+                label="Confirm password"
+                type="confirm-password"
+                id="confirm-password"
+                error={!confirmPassword.isValid}
+                helperText={confirmPassword.isValid ? "" : "Password missmatch"}
+                onChange={e => setConfirmPassword({ value: e.target.value, isValid: e.target.value === password.value })}
+              />
+            </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign Up
+          </Button>
+          <Grid item>
+            <Link href="#" variant="body2">
+              Already have an account? Sign in
+            </Link>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
